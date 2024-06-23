57b05179ce449a46a3e2225886f8ec48dce213f8
vadim <lociX55Lim@gmail.com>
2024-06-23 15:27:06 +0300

    fix: task12

--- tasks/task12.go
+++ tasks/task12.go
@@ -2,27 +2,19 @@ package tasks
 
 import "fmt"
 
-// Решение, собственным множеством является множетсво, которое не является самим множеством или пустым
-// в решении возьмем и получим все уникальные элементы, а после уберем один из них в исходном множестве
-// и получим собственное множество
-// не сработает, когда у нас всего 1 элемент, так как мы не сможем получить не пустое множество или множество не совпадающее
-// с исходным
+// Я так понимаю, тут хотят увидеть сет
+// В сете у нас все эелементы уникальны, просто сделаем мапу, в которой ключом будет строка, а значение пустаю структура для экономии памяти
 
 func Solve12() {
 	elems := []string{"cat", "cat", "dog", "cat", "tree"}
-	mapOfAmount := make(map[string]int)
+	mapOfElems := make(map[string]struct{})
 
 	for _, elem := range elems {
-		mapOfAmount[elem]++
+		mapOfElems[elem] = *new(struct{})
 	}
 
 	var ans []string
-	count := 0
-	for key := range mapOfAmount {
-		if count == 0 {
-			count++
-			continue
-		}
+	for key := range mapOfElems {
 		ans = append(ans, key)
 	}
 
